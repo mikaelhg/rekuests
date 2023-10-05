bd732ef085ef7e0ebbe789187ea1dd0ac93708af
Mikael Gueck <gumi@iki.fi>
2023-10-05 23:06:23 +0300

    Upgrade libs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -23,13 +23,13 @@ dependencies {
     implementation("com.github.mizosoft.methanol:methanol:1.7.0")
     implementation("io.mikael:urlbuilder:2.0.9")
 
-    testImplementation(enforcedPlatform("org.slf4j:slf4j-parent:2.0.7"))
-    testImplementation(enforcedPlatform("ch.qos.logback:logback-parent:1.4.8"))
+    testImplementation(enforcedPlatform("org.slf4j:slf4j-parent:2.0.9"))
+    testImplementation(enforcedPlatform("ch.qos.logback:logback-parent:1.4.11"))
     testImplementation(enforcedPlatform("org.eclipse.jetty:jetty-project:11.0.15"))
 
     testImplementation("ch.qos.logback:logback-classic")
-    testImplementation("org.slf4j:jul-to-slf4j:2.0.7")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.slf4j:jul-to-slf4j:2.0.9")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation("io.javalin:javalin:5.6.1")
