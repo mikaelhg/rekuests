07107a95579c5b275837e26290afe4a379ca36ba
Mikael Gueck <gumi@iki.fi>
2025-05-03 01:14:08 +0300

    fix: Upgrade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_20
-import org.jetbrains.kotlin.gradle.dsl.jvm.JvmTargetValidationMode.IGNORE
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 plugins {
     `java-library`
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.1.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 group = "io.mikael"
@@ -23,24 +20,24 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.5.1")
-    implementation("com.github.mizosoft.methanol:methanol:1.7.0")
+
+    implementation("com.github.mizosoft.methanol:methanol:1.8.2")
     implementation("io.mikael:urlbuilder:2.0.9")
 
-    testImplementation(enforcedPlatform("org.slf4j:slf4j-parent:2.0.9"))
-    testImplementation(enforcedPlatform("ch.qos.logback:logback-parent:1.4.11"))
-    testImplementation(enforcedPlatform("org.eclipse.jetty:jetty-project:11.0.15"))
+    testImplementation(enforcedPlatform("org.slf4j:slf4j-parent:2.0.17"))
+    testImplementation(enforcedPlatform("ch.qos.logback:logback-parent:1.5.18"))
+    testImplementation(enforcedPlatform("org.eclipse.jetty:jetty-project:11.0.25"))
 
     testImplementation("ch.qos.logback:logback-classic")
-    testImplementation("org.slf4j:jul-to-slf4j:2.0.9")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.slf4j:jul-to-slf4j:2.0.17")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    testImplementation("io.javalin:javalin:5.6.3")
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    testImplementation("io.javalin:javalin:6.6.0")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
 
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.4.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.17.0")
 }
 
 tasks {
